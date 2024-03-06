corrected E_sk in cylinder.py added units to print output changed graph x-axis units from seconds to hours

calculation/2024-02 - Oddone/0 - First Try.py
@@ -1,11 +1,12 @@
-#%% ----- IMPORT MODULES -----
+# %% ----- IMPORT MODULES -----
 
 from main_code.cylinder_model.cylinder import Cylinder, CylinderGeometry, CylinderCoefficients, EnvironmentalConditions
 from main_code.body_class import Body
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import numpy as np
 
 
-#&& ----- PLOTS -----
+# && ----- PLOTS -----
 
 def simulate_temperature_evolution(cylinder, delta_t, max_steps=10):
     times = []
@@ -22,72 +23,34 @@ def simulate_temperature_evolution(cylinder, delta_t, max_steps=10):
 
     return times, temperatures
 
-#%% ----- OBJECT'S CREATION
+
+# %% ----- OBJECT'S CREATION
 tommaso = Body()
 tommaso_env_conditions = EnvironmentalConditions(tommaso)
-tommaso_env_conditions.set_conditions(temperature=299.15, pressure = 101325, humidity = 0.5)
+tommaso_env_conditions.set_conditions(temperature=299.15, pressure=101325, humidity=0.5)
 tommaso_env_conditions.calculate_properties()
-coefficent_cylinder = CylinderCoefficients(tommaso,tommaso_env_conditions)
-geometry_cylinder = CylinderGeometry(d=0.2, h=0.61, s=0.2)
-trunk = Cylinder(geometry_cylinder, tommaso, coefficent_cylinder, tommaso_env_conditions)
+coefficient_cylinder = CylinderCoefficients(tommaso, tommaso_env_conditions)
+geometry_cylinder = CylinderGeometry(d=0.2, h=0.8, s=0.2)
+trunk = Cylinder(geometry_cylinder, tommaso, coefficient_cylinder, tommaso_env_conditions)
 
 delta_t = 1  # Intervallo di tempo in secondi
-times, temperatures = simulate_temperature_evolution(trunk, delta_t, max_steps=60*60*24)
+times, temperatures = simulate_temperature_evolution(trunk, delta_t, max_steps=60 * 60 * 24)
 
-
-plt.plot(times, temperatures, marker='o', linestyle='-')
-plt.xlabel('Time (s)')
+plt.plot(times, temperatures, linestyle='-')
+plt.xticks(np.arange(0, len(times) + 1, step=60 * 60 * 2), labels=np.arange(0, 25, 2))
+plt.xlabel('Time (hr)')
 plt.ylabel('Internal Temperature (K)')
 plt.title('Change in internal temperature over time')
 plt.grid(True)
 plt.show()
 
-#%% ---- TEST CODE -----
+# %% ---- TEST CODE -----
 
-print ('il valore del calore scambiato per conduzione vale: ',trunk.Q_cond())
-print('il valore del calore scambiato per convezione vale: ', trunk.Q_conv())
-print('il valore del calore scambiato per irraggiamento vale: ', trunk.Q_irr())
-print('il valore del calore perso per evaporazione vale: ', trunk.E_sk())
-print('il valore del calore scambiato attraverso il sangue vale: ',trunk.Q_blood())
-print('il valore del lavoro utilizzando per pompare il sangue vale: ',trunk.W_pump_blood())
-print('il valore finale della T_int è: ', trunk.energy_balance(delta_t=60*60))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print('il valore del calore scambiato per conduzione vale: ', trunk.Q_cond(), '[W]')
+print('il valore del calore scambiato per convezione vale: ', trunk.Q_conv(), '[W]')
+print('il valore del calore scambiato per irraggiamento vale: ', trunk.Q_irr(), '[W]')
+print('il valore del calore perso per evaporazione vale: ', trunk.E_sk(), '[W]')
+print('il valore del calore scambiato attraverso il sangue vale: ', trunk.Q_blood(), '[W]')
+print('il valore del lavoro utilizzando per pompare il sangue vale: ', trunk.W_pump_blood(), '[W]')
+print('il valore finale della T_int è: ', trunk.energy_balance(delta_t=60*60), '[K]')
 

main_code/cylinder_model/support/environmental_conditions.py
@@ -1,8 +1,11 @@
 from CoolProp.CoolProp import PropsSI, HAPropsSI
 import numpy as np
 from main_code.body_class import Body
+
+
 class EnvironmentalConditions:
-    def __init__(self, body : Body, temperature=298, pressure=101325, humidity=0.50,v_air = 0.6, properties=None, fluid='water'):
+    def __init__(self, body: Body, temperature=298, pressure=101325, humidity=0.50, v_air=0.6, properties=None,
+                 fluid='water'):
         self.body = body
         self.fluid = fluid
         self.temperature = temperature
@@ -11,7 +14,7 @@ class EnvironmentalConditions:
         self.properties = properties
         self.v_air = v_air
 
-    def set_conditions(self,temperature,pressure,humidity):
+    def set_conditions(self, temperature, pressure, humidity):
         self.temperature = temperature
         self.pressure = pressure
         self.humidity = humidity
@@ -25,24 +28,24 @@ class EnvironmentalConditions:
         thermal_conductivity = PropsSI('L', 'T', self.temperature, 'P', self.pressure, self.fluid)
         water_vapor_pressure = PropsSI('P', 'T', self.temperature, 'Q', 1, 'Water')
         water_vapor_pressure_skin = PropsSI('P', 'T', self.body.T_skin, 'Q', 1, 'Water')
-        humidity_ratio = HAPropsSI('W','T',self.temperature, 'P', self.pressure, 'RH', self.humidity)
-        wet_bulb_temperature = HAPropsSI('Twb','T', self.temperature, 'P', self.pressure, 'RH', self.humidity)
+        humidity_ratio = HAPropsSI('W', 'T', self.temperature, 'P', self.pressure, 'RH', self.humidity)
+        wet_bulb_temperature = HAPropsSI('Twb', 'T', self.temperature, 'P', self.pressure, 'RH', self.humidity)
         water_vapor_pressure_37_degrees = PropsSI('P', 'T', 310.15, 'Q', 1, 'Water')
 
         self.properties = {
-            'Temperature': self.temperature
-            , 'Pressure': self.pressure
-            , 'Humidity': self.humidity
-            , 'Density': density
-            , 'Enthalpy': enthalpy
-            , 'Entropy': entropy
-            , 'Specific Heat': specific_heat
-            , 'Dynamic Viscosity': dynamic_viscosity
-            , 'Thermal Conductivity': thermal_conductivity
-            , 'Water Vapor Pressure': water_vapor_pressure
-            , 'Water Vapor Pressure Skin': water_vapor_pressure_skin
-            , 'Humidity Ratio': humidity_ratio
-            , 'Wet Bulb Temperature': wet_bulb_temperature
+            'Temperature':                       self.temperature
+            , 'Pressure':                        self.pressure
+            , 'Humidity':                        self.humidity
+            , 'Density':                         density
+            , 'Enthalpy':                        enthalpy
+            , 'Entropy':                         entropy
+            , 'Specific Heat':                   specific_heat
+            , 'Dynamic Viscosity':               dynamic_viscosity
+            , 'Thermal Conductivity':            thermal_conductivity
+            , 'Water Vapor Pressure':            water_vapor_pressure
+            , 'Water Vapor Pressure Skin':       water_vapor_pressure_skin
+            , 'Humidity Ratio':                  humidity_ratio
+            , 'Wet Bulb Temperature':            wet_bulb_temperature
             , 'Water Vapor Pressure 37 degrees': water_vapor_pressure_37_degrees
         }
 
@@ -50,26 +53,29 @@ class EnvironmentalConditions:
         return self.properties
 
     def get_vector_a(self):
-        return np.array([0.5, 0.5, 3.5, 3.5, 3.5, 3.5])    #[up, down, left, right, front, back]
+        return np.array([0.5, 0.5, 3.5, 3.5, 3.5, 3.5])  # [up, down, left, right, front, back]
+
     def get_vector_b(self):
         return np.array([3.5, 3.5, 4.5, 4.5, 4.5, 4.5])
+
     def get_vector_c(self):
         return np.array([3.5, 0.5, 0.5, 4.5, 3.5, 0.5])
-    def calculate_area_factor(self,vector_a,vector_b,vector_c):
-        x_v = vector_a/vector_b
-        y_v = vector_b/vector_c
-        x_h = vector_c/vector_b
-        y_h = vector_a/vector_b
+
+    def calculate_area_factor(self, vector_a, vector_b, vector_c):
+        x_v = vector_a / vector_b
+        y_v = vector_b / vector_c
+        x_h = vector_c / vector_b
+        y_h = vector_a / vector_b
 
         F_dv = (1 / (2 * np.pi)) * ((x_v / np.sqrt(1 + x_v ** 2)) * np.arctan(y_v / np.sqrt(1 + x_v ** 2)) + (
                 y_v / np.sqrt(1 + y_v ** 2)) * np.arctan(x_v / np.sqrt(1 + y_v ** 2)))
         F_dh = (1 / (2 * np.pi)) * (np.arctan(1 / y_h) + (y_h / np.sqrt(x_h ** 2 + y_h ** 2)) * np.arctan(
-                1 / np.sqrt(x_h ** 2 + y_h ** 2)))
+            1 / np.sqrt(x_h ** 2 + y_h ** 2)))
 
-        area_factor=[]
+        area_factor = []
 
         for i in range(len(vector_a)):
-            if i <2:
+            if i < 2:
                 area_factor.append(F_dv[i])
             else:
                 area_factor.append(F_dh[i])
@@ -77,96 +83,98 @@ class EnvironmentalConditions:
         return area_factor
 
     def get_vector_T_surf(self):
-        return np.array([20, 18, 19, 20, 21.5, 22]) #wall temperatures
-    def calculate_T_mr(self,area_factor,T_surf):
+        return np.array([20, 18, 19, 20, 21.5, 22])  # wall temperatures
+
+    def calculate_T_mr(self, area_factor, T_surf):
         return (np.sum(np.fromiter((T_surf[i] ** 4 * area_factor[i] for i in range(len(area_factor))), dtype=float)) /
-                np.sum(np.fromiter((area_factor[i] for i in range(len(area_factor))), dtype=float))) ** (1 / 4)   #[°C]
+                np.sum(np.fromiter((area_factor[i] for i in range(len(area_factor))), dtype=float))) ** (1 / 4)  # [°C]
+
     def get_inspired_air_composition(self):
         comp = {
-            'pCO2': 0.03,   #%
-            'pO2': 20.93,   #%
-            'pN2': 79.04    #%
+            'pCO2': 0.03,  # %
+            'pO2':  20.93,  # %
+            'pN2':  79.04  # %
         }
         return comp
+
     def get_expired_air_composition(self):
         comp = {
             'pCO2': 3.60,  # %
-            'pO2': 16.86,  # %
-            'pN2': 79.54   # %
+            'pO2':  16.86,  # %
+            'pN2':  79.54  # %
         }
         return comp
+
     def get_Pt_torr(self):
-        return 760  #[Torr]; 760 [Torr] = 101325 [Pa]
+        return 760  # [Torr]; 760 [Torr] = 101325 [Pa]
+
     def get_V_atps(self):
-        return 30   #[l]; volume of expired gas
+        return 30  # [l]; volume of expired gas
+
     def calculate_V_st(self):
-        return self.get_V_atps()*(273.15/self.temperature)    #[l]; gas volume at standard temperature
+        return self.get_V_atps() * (273.15 / self.temperature)  # [l]; gas volume at standard temperature
+
     def calculate_V_sp(self):
-        return self.get_V_atps()*(self.get_Pt_torr()/760)    #[l]; gas volume at standard pressure
+        return self.get_V_atps() * (self.get_Pt_torr() / 760)  # [l]; gas volume at standard pressure
 
     def calculate_V_spd(self):
-        water_vapor_pressure_torr = (self.properties['Water Vapor Pressure'])/133.3    #[torr]
-        return self.get_V_atps()*((self.get_Pt_torr()-water_vapor_pressure_torr)/760)    #[l]; gas volume at standard pressure dry
+        water_vapor_pressure_torr = (self.properties['Water Vapor Pressure']) / 133.3  # [torr]
+        return self.get_V_atps() * (
+                    (self.get_Pt_torr() - water_vapor_pressure_torr) / 760)  # [l]; gas volume at standard pressure dry
 
     def get_water_vapor_pressure_at_37_degrees_torr(self):
-        water_vapor_pressure_37_degrees = (self.properties['Water Vapor Pressure 37 degrees'])/133.3    #[torr]
-        return water_vapor_pressure_37_degrees  #[Torr]
+        water_vapor_pressure_37_degrees = (self.properties['Water Vapor Pressure 37 degrees']) / 133.3  # [torr]
+        return water_vapor_pressure_37_degrees  # [Torr]
 
     def calculate_V_stpd(self):
         water_vapor_pressure_torr = (self.properties['Water Vapor Pressure']) / 133.3  # [torr]
-        return self.get_V_atps()*((self.get_Pt_torr()-water_vapor_pressure_torr)/760)*(273.15/self.temperature)   #[l]; gas volume
+        return self.get_V_atps() * ((self.get_Pt_torr() - water_vapor_pressure_torr) / 760) * (
+                    273.15 / self.temperature)  # [l]; gas volume
 
     def calculate_V_btps(self):
-        water_vapor_pressure_torr = (self.properties['Water Vapor Pressure']) / 133.3   # [torr]
+        water_vapor_pressure_torr = (self.properties['Water Vapor Pressure']) / 133.3  # [torr]
         water_vapor_pressure_37_degrees = (self.properties['Water Vapor Pressure 37 degrees']) / 133.3  # [torr]
-        return (self.get_V_atps() * (310.15/self.temperature)
-                * ((self.get_Pt_torr() - water_vapor_pressure_torr)/(self.get_Pt_torr() -water_vapor_pressure_37_degrees )))
+        return (self.get_V_atps() * (310.15 / self.temperature)
+                * ((self.get_Pt_torr() - water_vapor_pressure_torr) / (
+                            self.get_Pt_torr() - water_vapor_pressure_37_degrees)))
 
     def get_V_e(self):
-        return 26.483   #[l]; volume of expired air
+        return 26.483  # [l]; volume of expired air
 
     def calculate_V_i(self):
         comp_insp = self.get_inspired_air_composition()
         comp_exp = self.get_expired_air_composition()
-        return (self.get_V_e()*(comp_exp['pN2']/100 * self.get_V_e()))/(comp_insp['pN2']/100 * self.get_V_e())  #[l]; volume of inspired air
+        return (self.get_V_e() * (comp_exp['pN2'] / 100 * self.get_V_e())) / (
+                    comp_insp['pN2'] / 100 * self.get_V_e())  # [l]; volume of inspired air
 
     def calculate_V_O2_i(self):
         comp_insp = self.get_inspired_air_composition()
-        return comp_insp['pO2']/100 * self.calculate_V_i()    #[l]; volume of O2 in inspired air
+        return comp_insp['pO2'] / 100 * self.calculate_V_i()  # [l]; volume of O2 in inspired air
 
     def calculate_VO2_e(self):
         comp_exp = self.get_expired_air_composition()
-        return comp_exp['pO2']/100*self.get_V_e()   #[l]; volume of O2 in expired air
+        return comp_exp['pO2'] / 100 * self.get_V_e()  # [l]; volume of O2 in expired air
 
     def calculate_VO2(self):
-        return self.calculate_V_O2_i()-self.calculate_VO2_e()    #[l]; volume of O2 removed from inspired air
+        return self.calculate_V_O2_i() - self.calculate_VO2_e()  # [l]; volume of O2 removed from inspired air
 
     def calculate_VCO2(self):
         comp_insp = self.get_inspired_air_composition()
         comp_exp = self.get_expired_air_composition()
-        return (comp_exp['pCO2']/100 * self.get_V_e())-(comp_insp['pCO2']/100*self.calculate_V_i())   #[l]; volume of carbon dioxide produced
+        return (comp_exp['pCO2'] / 100 * self.get_V_e()) - (
+                    comp_insp['pCO2'] / 100 * self.calculate_V_i())  # [l]; volume of carbon dioxide produced
 
     def get_Q_O2_for_minute(self):
         return self.calculate_VO2()
+
     def get_Q_CO2_for_minute(self):
         return self.calculate_VCO2()
 
     def calculate_RQ(self):
-        return self.get_Q_CO2_for_minute()/self.get_Q_O2_for_minute()   #respiratory quotient
+        return self.get_Q_CO2_for_minute() / self.get_Q_O2_for_minute()  # respiratory quotient
 
     def calculate_O2_true(self):
         comp_insp = self.get_inspired_air_composition()
         comp_exp = self.get_expired_air_composition()
-        return ((comp_exp['pN2']/100)/(comp_insp['pN2']/100)*(comp_insp['pO2']/100))-(comp_exp['pO2'] / 100)    #percentage of O2 consumed for any volume of air expired
-
-
-
-
-
-
-
-
-
-
-
-
+        return ((comp_exp['pN2'] / 100) / (comp_insp['pN2'] / 100) * (comp_insp['pO2'] / 100)) - (
+                    comp_exp['pO2'] / 100)  # percentage of O2 consumed for any volume of air expired